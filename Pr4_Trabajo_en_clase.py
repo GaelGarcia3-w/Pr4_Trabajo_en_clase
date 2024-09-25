381a8ab69b5ea14c86ea92cd7d94b4174398e7dc
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-09-24 19:55:10 -0600

    Pr4_Trabajo_en_clase

---
+++ Pr4_Trabajo_en_clase.py
@@ -0,0 +1,15 @@
+print(" ")
+
+print("Edgar Gael Garcia Camacho :Pr4_Trabajo_en_clase : ")
+
+print(" ")
+
+F=int(input("Ingresa la temperatura Fahrenheit :"))#Tepide ingresar la temperatura Fahrenheit
+
+r=(5/9)#la operacion para sacar los grados Fahrenheit
+
+r1= (F-32)
+
+c=(r * r1)#Operacion para sacar los grados celsius
+
+print("Los grados Fahrenheit son :", r , "Los grados celsius son :", c)#Aqui te mostrara un texto con los grados Fahrenheit y los grados celsius.
